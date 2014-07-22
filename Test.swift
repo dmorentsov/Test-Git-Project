ebbe84628d7c9e0cc5a38264b4768b509500c9cc
Denis Morentsov <dmorentsov@backstopsolutions.com>
2014-07-21 22:43:48 -0500

    Edit test class

---
+++ Test.swift
@@ -0,0 +1,13 @@
+//
+//  Test.swift
+//  Git Test
+//
+//  Created by Denis Morentsov on 7/21/14.
+//  Copyright (c) 2014 Exelsis. All rights reserved.
+//
+
+import UIKit
+
+class Test: NSObject {
+   
+}
